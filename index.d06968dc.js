4a56303c6951e34c73223a1ae70dbcb3221d5920
NataliaTopornitska <natalia.topornitska3@gmail.com>
2024-07-02 14:17:41 +0300

    make build

---
+++ index.d06968dc.js
@@ -0,0 +1,2 @@
+document.addEventListener("DOMContentLoaded",function(){var e=document.querySelector(".wall"),t=document.querySelector(".spider"),n=e.getBoundingClientRect(),o=t.getBoundingClientRect(),c=(n.width-o.width)/2,d=(n.height-o.height)/2;t.style.left="".concat(c,"px"),t.style.top="".concat(d,"px")});
+//# sourceMappingURL=index.d06968dc.js.map
